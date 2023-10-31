75b927f0ec20b7e1fadb45536cfedddeee349d0b
PC <juan.ciancio@consultores.wom.cl>
2023-10-31 18:38:12 -0300

    rama con errores en el use effect para entender el renderizado

--- src/app/componentes/card/Card.tsx
+++ src/app/componentes/card/Card.tsx
@@ -1,25 +1,48 @@
 "use client";
-import './Card.css';
+import { useState } from "react";
+import "./Card.css";
 export const Card = (props: any) => {
- 
-  const { titulo, postulacionClicked } = props;
+  const { titulo, postulacionClicked, postulado } = props;
+  const [btnPostulacion, setBtnPostulacion] = useState<string>(
+    postulado ? "despostular" : "postular"
+  );
+  // const [sePostulo, setSePostulo] = useState<boolean>(false);
 
+  /*   const postular = (titulo: string) => {
+    if (!sePostulo) {
+      postulacionClicked(titulo);
+      setSePostulo(true);
+    } else {
+      setSePostulo(false);
+      console.log("despostular");
+    }
+  }; */
   // const postularAEmpleo = (event:React.SyntheticEvent) => {
   //   console.log(event);
   //   postulacionClicked(titulo);
   // }
 
+  /*   useEffect(() => {
+    if (sePostulo) {
+      setBtnPostulacion("despostular");
+    } else {
+      setBtnPostulacion("POSTULAR");
+    }
+  }, [sePostulo]); */
+
   return (
-    <div className='cart-job'>
+    <div className="cart-job">
       <h2>{titulo}</h2>
       <hr />
       <button
         className="btn btn-info"
+        key={titulo}
         // onClick={(event) => postularAEmpleo(event)}
         onClick={() => postulacionClicked(titulo)}
       >
-        Postulate!!
-        </button>
+        {btnPostulacion}
+      </button>
+      {/* {postulado && <CantidadTomados />} */}
     </div>
-  )
-}
+  );
+};
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,28 +1,24 @@
-"use client"
-import { useState, useEffect } from "react"
-import { BarraLateral } from "./componentes/barralateral/BarraLateral"
-import { Card } from "./componentes/card/Card"
-import { CardContainer } from "./componentes/cardContainer/CardContainer"
-import { Encabezado } from "./componentes/encabezado/Encabezado"
+"use client";
+import { useEffect, useState } from "react";
+import { BarraLateral } from "./componentes/barralateral/BarraLateral";
+import { Card } from "./componentes/card/Card";
+import { CardContainer } from "./componentes/cardContainer/CardContainer";
+import { Encabezado } from "./componentes/encabezado/Encabezado";
 
 export default function Home() {
-
-  const options = ["Mis Empleos", "Mi Cuenta", "Configuracion"]
-
-  // const [nombreDelEstado, nombreFuncionActualizacionEstado] = useState(ValorInicial);
+  const options = ["Mis Empleos", "Mi Cuenta", "Configuracion"];
   const [jobs, setJobs] = useState<string[]>([]);
   const [jobsTomados, setJobsTomados] = useState<string[]>([]);
   const [remanentes, setRemanentes] = useState(0);
   const [cantTomados, setCantTomados] = useState(0);
 
-
-  const postulacion = (titulo:string) => {
+  const postulacion = (titulo: string) => {
     const newJobs = jobs.filter((job) => job != titulo);
     setJobs(newJobs);
 
     //  ...algo es el contenido de, sirve para objetos o arreglos
     // en este caso estoy creando un arreglo nuevo con el contenido de jobsTomados y titulo
-    const tomados:string[] = [...jobsTomados, titulo];
+    const tomados: string[] = [...jobsTomados, titulo];
     //hacer esto es equivalente a
     // const tomados = [];
     // jobsTomados.forEach((jt) => {
@@ -30,21 +26,25 @@ export default function Home() {
     // });
     // tomados.push(titulo);
     setJobsTomados(tomados);
-  }
+  };
 
   const resetPostulaciones = () => {
     setJobs(["Desarrollador", "Project Manager", "QA", "Analista Software"]);
     setJobsTomados([]);
-
-  }
+  };
 
   // cuando se pone el arreglo vacio como disparador, se llama al callback despues del primer render
   useEffect(() => {
-    const misJobs =  ["Desarrollador", "Project Manager", "QA", "Analista Software"];
+    const misJobs = [
+      "Desarrollador",
+      "Project Manager",
+      "QA",
+      "Analista Software",
+    ];
     setJobs(misJobs);
   }, []);
 
-  //cuando se pone el arreglo con uno o mas estados (jobs) disparador, 
+  //cuando se pone el arreglo con uno o mas estados (jobs) disparador,
   // se llama al callback despues que se ejecuta el set de estado
   useEffect(() => {
     setRemanentes(jobs.length);
@@ -64,40 +64,44 @@ export default function Home() {
             <CardContainer>
               <h3>Postulaciones Disponibles</h3>
               <div className="d-flex flex-row justify-content-evenly">
-                {jobs.map((job:string) => (
-                  <Card 
-                    titulo={job} 
-                    key={`jobItem${job}`}  
-                    postulacionClicked={(titulo:string) => postulacion(titulo)}
+                {jobs.map((job: string) => (
+                  <Card
+                    titulo={job}
+                    key={`jobItem${job}`}
+                    postulacionClicked={(titulo: string) => postulacion(titulo)}
+                    /* postulado={false} */
                   />
                 ))}
               </div>
-            </CardContainer> 
+            </CardContainer>
             <CardContainer>
               Quedan disponibles {remanentes} postulaciones
               <br />
-               Cantidad postulaciones {cantTomados} realizadas
+              Cantidad postulaciones {cantTomados} realizadas
               <br />
               <button
                 className="btn btn-secondary"
                 onClick={() => resetPostulaciones()}
-                >Cancelar</button>
+              >
+                Cancelar
+              </button>
             </CardContainer>
             <CardContainer>
-            <h3>Postulaciones Tomadas</h3>
+              <h3>Postulaciones Tomadas</h3>
               <div className="d-flex flex-row justify-content-evenly">
-                {jobsTomados.map((job:string) => (
-                  <Card 
-                    titulo={job} 
-                    key={`jobItem${job}`}  
-                    postulacionClicked={(titulo:string) => postulacion(titulo)}
+                {jobsTomados.map((job: string) => (
+                  <Card
+                    titulo={job}
+                    key={`jobItem${job}`}
+                    postulacionClicked={(titulo: string) => postulacion(titulo)}
+                    /* postulado={true} */
                   />
                 ))}
               </div>
-            </CardContainer> 
+            </CardContainer>
           </div>
         </div>
       </main>
     </>
-  )
+  );
 }
